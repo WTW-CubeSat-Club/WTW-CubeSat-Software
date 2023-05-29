fd4610d190e88aa0db8b0bd636f2dd52e90c1862
zw <zwanna1508@gmail.com>
2023-05-29 01:31:44 -0400

    code is now completely functional, need to add security and better formatting

--- client.py
+++ client.py
@@ -1,20 +1,23 @@
-#!/usr/bin/env python
-
 import asyncio
 import websockets
+ 
+async def test():
+    async with websockets.connect('ws://localhost:8000') as websocket:
+        while True:    
+            command = input("Command: ")
+            await websocket.send(command)
+            start_time = input("Start time: ")
+            await websocket.send(start_time)
+            time = input("End time: ")
+            await websocket.send(time)
+            data_type = input("Data type: ")
+            await websocket.send(data_type)
+            print("\n")
+            data = await websocket.recv()
+            print(data)
+            print("done")
+            quit()
+        
+asyncio.get_event_loop().run_until_complete(test())
 
-async def hello():
-    uri = "ws://localhost:8765"
-    async with websockets.connect(uri) as websocket:
-        time = input("Start time, end time: ")
-        await websocket.send(time)
-        data_type = input("Data type: ")
-        await websocket.send(data_type)
-
-        await websocket.recv()
-        await asyncio.Future()
-
-
-
-if __name__ == "__main__":
-    asyncio.run(hello())
+asyncio.get_event_loop().run_forever()
--- sqlquery.py
+++ sqlquery.py
@@ -14,8 +14,8 @@ c = conn.cursor()
 #? represents variable values
 #c.execute("INSERT INTO temp VALUES (?, ?)", (unix_time, data_value))
 
-c.execute("SELECT * FROM temp")
-print(c.fetchall())
+#c.execute("SELECT * FROM temp")
+#print(c.fetchall())
 
 #commit
 #conn.commit()
@@ -28,22 +28,33 @@ class sql:
     table = ""
     #no init method needed
     def get(self, start_time, end_time, data_type):
+        data_list =  ""
         #find data_type and set which table we are querying, using single quotes so sqlite doesn't get confused
         if data_type == "temp":
             while start_time <= end_time:
                 #convert into str because sqlite is picky like that
                 str_time = "%s" % start_time
-                #print(str_time)
+                print(str_time)
                 c.execute("""SELECT unix_time, data FROM temp WHERE unix_time = ?""", [str_time])
-                print(c.fetchall())
+                data = c.fetchall()
+                datastr = f"{str_time}\n{str(data)}\n"
                 start_time+=1
+                print(datastr)
+                data_list+=datastr
+
         if data_type == "altitude":
             while start_time <= end_time:
                 #convert into str because sqlite is picky like that
                 str_time = "%s" % start_time
                 c.execute("""SELECT * FROM altitude WHERE unix_time = ?""", [str_time])
                 data = c.fetchall()
+                datastr = f"{str_time}\n{str(data)}\n"
+                print(datastr)
                 start_time+=1
+                print(data)
+                data_list+=datastr
+
+        return data_list        
 
     def append(self, data_type, data):
         #using if statements so I don't have to interpolate the data_type
@@ -58,10 +69,10 @@ class sql:
         """
         conn.commit()
 
-console = sql()
-console.get(1684418411, 1684418491, "temp")
-console.append("temp", 96.42)
-c.execute("SELECT * FROM temp")
+#console = sql()
+#console.get(1685335865, 1685335870, "temp")
+#console.append("temp", 96.42)
+#c.execute("SELECT * FROM temp")
 #print(c.fetchall())
 
-conn.close()
+#conn.close()
