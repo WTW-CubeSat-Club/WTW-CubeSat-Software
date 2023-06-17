a9709bdc8d8e8dfee2836d348ef5c60d8ef4957b
zw <zwanna1508@gmail.com>
2023-06-17 15:35:11 -0400

    finished sattracker and did some db restructuring

--- client.py
+++ client.py
@@ -2,18 +2,19 @@ import asyncio
 import websockets
 import pathlib
 import ssl
+import sys
 import subprocess
-import os
 import matplotlib.pyplot as plt
 import csv
 import time
+
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 localhost_pem = pathlib.Path(__file__).with_name("test.pem")
 ssl_context.load_verify_locations(localhost_pem)
 
 def clear():
-    if os.name == "nt":
+    if sys.platform == "win32" or sys.platform == "cygwin" or sys.platform == "msys":
         subprocess.run("cls")
     else:
         subprocess.run("clear")
@@ -21,10 +22,12 @@ def clear():
 async def socket():
     global error
     error = True
+    
     try:
         async with websockets.connect('wss://localhost:8000', ssl=ssl_context) as websocket:
-            while True:    
-                command = input("Command: ")
+            while True:
+                clear()
+                command = input("\nCommand: ")
                 await websocket.send(command)
                 global start_time
                 start_time = input("Start time: ")
@@ -37,12 +40,20 @@ async def socket():
                 unparsed = await websocket.recv()
                 error = False
                 break
+    
+    #display error if connect fails
+    except OSError:
+        clear()
+        print("\n[Could  not connect]")
+        time.sleep(1.5)
+    
+    #display error for invalid params
     except:
         clear()
-        print("\n[Error occured, please try again]")
+        print("\n[Parameters are invalid, try again]")
         time.sleep(1.5)
-
         
+#parse y list given as a string
 def parse(list):
     reader = csv.reader(list.splitlines(), quoting=csv.QUOTE_NONNUMERIC)
     global y_list
@@ -50,6 +61,7 @@ def parse(list):
     global y_len
     y_len = len(y_list)
 
+#graph x and y lists
 def graph(x_list, y_list):
     ask = input("\nGraph data? [y/n]: ")
     if ask.lower() == "y":
@@ -58,7 +70,7 @@ def graph(x_list, y_list):
         plt.plot(x_list, y_list, 'o')
         plt.show()
     
-
+#make x list based on start time and end time
 def makeXList(start_time):
     x_list = []
     for i in range(y_len):
@@ -68,36 +80,42 @@ def makeXList(start_time):
     return x_list
 
 
-
+#save data
 def saveData(x_list, y_list):
-    ask = input("\nSave data? [y/n]: ")
-    if ask.lower() == 'y':
-        filename = input("Enter the filename: ")
-        #make sure it's saved as csv
-        filename = filename+".csv"
-        with open(filename, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(x_list)
-            writer.writerow(y_list)
-            clear()
-            print("\n[OK]")
-            time.sleep(1)
-    else:
-        print("Data not saved.")
+    try:
+        ask = input("\nSave data? [y/n]: ")
+        if ask.lower() == 'y':
+            filename = input("Enter the filename: ")
+            #make sure it's saved as csv
+            filename = filename+".csv"
+            with open(filename, 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(x_list)
+                writer.writerow(y_list)
+                clear()
+                print("\n[OK]")
+                time.sleep(1)
 
+    #error catching
+    except:
+        clear()
+        print("\n[Error, data not saved]")
+        time.sleep(1)
+
+#connect using socket function
 def connect():
     start = input("\n[Press a key to connect]")
     clear()
-    print("\n")
     asyncio.get_event_loop().run_until_complete(socket())
     clear()
-    
+
 
 def main():
     clear()
     try:
         while True:
             connect()
+
             if error == False:
                 print("\n[OK]")
                 time.sleep(1)
@@ -112,7 +130,7 @@ def main():
     except KeyboardInterrupt:
         clear()
         print("\n[Quitting]")
-        time.sleep(0.7)
+        time.sleep(0.6)
         clear()
         quit
 
--- sqlquery.py
+++ sqlquery.py
@@ -1,27 +1,9 @@
-import sqlite3
 import time
+import initalize
+import sqlite3
 
-#takes unix time and rounds to get an int
-#unix_time =  round(time.time())
 conn = sqlite3.connect("data.db")
-#data_value = 97
-
-
-#create cursor
 c = conn.cursor()
-
-
-#? represents variable values
-#c.execute("INSERT INTO temp VALUES (?, ?)", (unix_time, data_value))
-
-#c.execute("SELECT * FROM temp")
-#print(c.fetchall())
-
-#commit
-#conn.commit()
-
-#close
-#conn.close()
 
 def parse(data):
         unparsed_data = str(data)
@@ -42,8 +24,9 @@ class sql:
     #no init method needed
     def get(self, start_time, end_time, data_type):
         y_list =  ""
+        data_type = data_type.replace(" ", "")
         #find data_type and set which table we are querying, using single quotes so sqlite doesn't get confused
-        if data_type == "temp":
+        if data_type.lower() == "temp":
             while start_time <= end_time:
                 #convert into str because sqlite is picky like that
                 str_time = "%s" % start_time
@@ -54,11 +37,31 @@ class sql:
                 start_time+=1
                 
 
-        if data_type == "altitude":
+        if data_type.lower() == "altitude":
             while start_time <= end_time:
                 #convert into str because sqlite is picky like that
                 str_time = "%s" % start_time
                 c.execute("""SELECT data FROM altitude WHERE unix_time = ?""", [str_time])
+                data = c.fetchall()
+                y_list += str(parse(data))
+                print(y_list)
+                start_time+=1
+
+        if data_type.lower() == "airpressure":
+            while start_time <= end_time:
+                #convert into str because sqlite is picky like that
+                str_time = "%s" % start_time
+                c.execute("""SELECT data FROM airpressure WHERE unix_time = ?""", [str_time])
+                data = c.fetchall()
+                y_list += str(parse(data))
+                print(y_list)
+                start_time+=1
+
+        if data_type == "gps":
+            while start_time <= end_time:
+                #convert into str because sqlite is picky like that
+                str_time = "%s" % start_time
+                c.execute("""SELECT data FROM gps WHERE unix_time = ?""", [str_time])
                 data = c.fetchall()
                 y_list += str(parse(data))
                 print(y_list)
@@ -69,22 +72,32 @@ class sql:
         return y_list      
 
     def append(self, data_type, data):
-        #using if statements so I don't have to interpolate the data_type
-        if data_type == "temp":
+        #callable names
+        supported_names= "tempaltitudeairpressuregpsimagepicturetemperature"
+        #get rid of spaces
+        data_type = data_type.replace(" ", "")
+        if data_type.lower() == "temp":
             c.execute("""INSERT INTO temp VALUES (?, ?)""", (round(time.time()), data))
-        if data_type == "altitude":
+        if data_type.lower() == "altitude":
             c.execute("""INSERT INTO altitude VALUES (?, ?)""", (round(time.time()), data))
-        if data_type == "airpressure":
+        if data_type.lower() == "airpressure" or data_type.lower() == "air pressure":
             c.execute("""INSERT INTO temp VALUES (?, ?)""", (round(time.time()), data))
-        """  else:
-            print("Invalid data type")
-        """
-        conn.commit()
+        if data_type.lower() == "gps" :
+            c.execute("""INSERT INTO gps VALUES (?, ?)""", (round(time.time()), data))
+        #fix this later
+        if data_type.lower() == "image" or data_type.lower() == "pictures":
+            c.execute("""INSERT INTO  images VALUES (?, ?)""", (round(time.time()), data))
+        #support for one unsupported data type per db in the form of the "other" collom
+        if data_type not in supported_names:
+            c.execute("""INSERT INTO other images VALUES (?, ?)""", (round(time.time()), data))
+        else:
+            print("[Data type not in DB]")
+            conn.commit()
 
 
 
-#console = sql()
+console = sql()
 #console.get(1685335865, 1685335870, "temp")
-#console.append("temp", 96.42)
+console.append("", 96.42)
 #c.execute("SELECT * FROM temp")
 #print(c.fetchall())
