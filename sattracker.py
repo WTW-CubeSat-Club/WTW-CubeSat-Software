b37da5629036f0f7ad68f371e7173b9d70f48d50
zw <zwanna1508@gmail.com>
2023-06-18 08:50:27 -0400

    clean up, team fixed quit func

--- sattracker.py
+++ sattracker.py
@@ -44,48 +44,51 @@ def main():
 
             #nested try statement because connection error might occur multiple times
             try:
-                current_data_req = requests.get(
-                    url=f"https://api.n2yo.com/rest/v1/satellite/positions/{sat_id}/{lat}/{lng}/{elevation}/{sec}/&apiKey={api_key}")
-                pass_data_req = requests.get(
-                    url=f"https://api.n2yo.com/rest/v1/satellite/visualpasses/{sat_id}/{lat}/{lng}/{elevation}/{days}/{min_visibility}/&apiKey={api_key}")
-                pass_data = pass_data_req.json()
-                current_data = current_data_req.json()
+                try:
+                    current_data_req = requests.get(
+                        url=f"https://api.n2yo.com/rest/v1/satellite/positions/{sat_id}/{lat}/{lng}/{elevation}/{sec}/&apiKey={api_key}")
+                    pass_data_req = requests.get(
+                        url=f"https://api.n2yo.com/rest/v1/satellite/visualpasses/{sat_id}/{lat}/{lng}/{elevation}/{days}/{min_visibility}/&apiKey={api_key}")
+                    pass_data = pass_data_req.json()
+                    current_data = current_data_req.json()
             
 
-                try:
+                
                     satname = current_data["info"]["satname"]
                     satalt = round(int(current_data["positions"][0]["sataltitude"]) * 3280.8)
                     satlat = current_data["positions"][0]["satlatitude"]
                     satlng = current_data["positions"][0]["satlongitude"]
+                
+
+
+                    clear()
+                    print("\n[Tracker]\n")
+                    print(f"Satellite name: {satname}")
+                    print(f"NORAD ID: {sat_id}")
+                    print(f"Current coordinates: {satlat}, {satlng}")
+                    print(f"Current altitude: {satalt} ft\n")
+                    print("[Pass Prediction]\n")
+                    # get passes data
+           
+
+                    try:
+                        start_time = datetime.fromtimestamp(pass_data["passes"][0]["startUTC"]).strftime('%Y-%m-%d %H:%M:%S')
+                        duration = pass_data["passes"][0]["duration"]
+                        minutes = int(duration) // 60
+                        seconds = int(duration) % 60
+                        num_passes = pass_data["info"]["passescount"]
+                        #show data
+                        print(f"Future passes over the next {days} days: {num_passes}")
+                        print(f"Time of next pass: {start_time}")
+                        print(f"Duration of next pass: {minutes} mins, {seconds} secs")
+
+                    except:
+                        print("No available pass data for the next {days} days")
+                    time.sleep(update)
+
                 except (requests.exceptions.JSONDecodeError, KeyError, IndexError):
                     print("\n[Error occurred while parsing data]")
                     time.sleep(5)
-                    continue
-
-                clear()
-                print("\n[Tracker]\n")
-                print(f"Satellite name: {satname}")
-                print(f"NORAD ID: {sat_id}")
-                print(f"Current coordinates: {satlat}, {satlng}")
-                print(f"Current altitude: {satalt} ft\n")
-                print("[Pass Prediction]\n")
-                # get passes data
-           
-
-                try:
-                    start_time = datetime.fromtimestamp(pass_data["passes"][0]["startUTC"]).strftime('%Y-%m-%d %H:%M:%S')
-                    duration = pass_data["passes"][0]["duration"]
-                    minutes = int(duration) // 60
-                    seconds = int(duration) % 60
-                    num_passes = pass_data["info"]["passescount"]
-                    #show data
-                    print(f"Future passes over the next {days} days: {num_passes}")
-                    print(f"Time of next pass: {start_time}")
-                    print(f"Duration of next pass: {minutes} mins, {seconds} secs")
-
-                except:
-                    print("No available pass data for the next {days} days")
-                time.sleep(update)
 
             except requests.exceptions.RequestException:
                 secs = 10
@@ -94,7 +97,7 @@ def main():
                     print(f"\n[Could not connect, retrying in {secs} secs]")
                     secs -= 1
                     time.sleep(0.95)
-                    continue
+                
 
                     
     except KeyboardInterrupt:
